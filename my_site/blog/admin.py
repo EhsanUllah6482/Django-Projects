61c527c1b66b53f4a8a4500e78074ca12751457b
Ehsan Ullah <ehsanullahcs04@gmail.com>
2025-07-14 22:39:54 +0500

    message

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -7,7 +7,7 @@ admin.site.register(Author)
 admin.site.register(Tag)
 
 class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
     list_filter = ('author', 'date', 'tags')
     list_display = ('title', 'author', 'date', 'slug')
     search_fields = ('title', 'content')
-    prepopulated_fields = {'slug': ('title',)}
